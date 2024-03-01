236b02c5bccca6666a9fa7dd61073d2eff5920b2
HUMBLE25 <wlsrua1007@gmail.com>
2024-03-01 10:53:06 +0900

    create microPiles.tsx

--- src/pages/microPiles.tsx
+++ src/pages/microPiles.tsx
@@ -1,79 +1,99 @@
 import React from 'react';
-
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import styles from '../styles/microPiles.module.css';
 const MicroPiles: React.FC  = () => {
     return (
-        <div style={{ width: '100%', height: '100%', position: 'relative', background: 'white' }}>
-            <div style={{ width: 1440, height: 727, left: 0, top: 104, position: 'absolute', background: 'white' }}>
-                <img style={{ width: 1440, height: 1030, left: 0, top: 0, position: 'absolute' }} src="https://via.placeholder.com/1440x1030" />
-                <div style={{ width: 1440, height: 727, left: 0, top: 0, position: 'absolute', background: 'rgba(0, 0, 0, 0.40)' }} />
-                <div style={{ width: 250, height: 50, left: 595, top: 339, position: 'absolute', color: 'white', fontSize: 64, fontFamily: 'Nanum Gothic', fontWeight: '700', wordWrap: 'break-word' }}>사업소개</div>
-                <div style={{ width: 800, height: 75, left: 320, top: 652, position: 'absolute', background: 'white' }}>
-                    <div style={{ width: 400, height: 75, left: 400, top: 0, position: 'absolute', background: 'rgba(0, 0, 0, 0.80)' }} />
-                    <div style={{ width: 270, height: 28, left: 69, top: 24, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Nanum Gothic', fontWeight: '700', wordWrap: 'break-word' }}>MICRO PILES 공법</div>
-                    <div style={{ width: 260, height: 28, left: 470, top: 24, position: 'absolute', color: 'white', fontSize: 32, fontFamily: 'Nanum Gothic', fontWeight: '700', wordWrap: 'break-word' }}>HELIX PILES 공법</div>
+        <div className={styles.wrapper}>
+            <Header />  
+            {/* 배너 */}
+            <div className={styles.banner}>
+                <div className={styles.banner_img} style={{backgroundImage:`url(/)`}}/>
+                <div className={styles.banner_overlay} />
+                <div className={styles.banner_title}>사업소개</div>
+                <div className={styles.banner_menu}>
+                    <div className={styles.banner_menuItem1}>MICRO PILES 공법</div>
+                    <div className={styles.banner_menuItem2}>HELIX PILES 공법</div>
                 </div>
             </div>
-            <div style={{ width: 1440, height: 110, left: 0, top: 1073, position: 'absolute' }}>
-                <div style={{ width: 1300, height: 110, left: 70, top: 0, position: 'absolute', background: 'rgba(222, 234, 246, 0.50)' }} />
-                <div style={{ width: 980, height: 52, left: 235, top: 29, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>기존건물의 기초보강, 굴착장비의 진입이 불가능한 협소한 지역 시공시 연약지반, 사면의 보강 등에 적합한 파일 공법</div>
+
+            {/* 개념*/}
+            <div className={styles.concept_container}>
+                <div className={styles.concept_title}>MICRO PILES 공법</div>
+                <div className={styles.concept_background}>기존건물의 기초보강, 굴착장비의 진입이 불가능한 협소한 지역 시공시 연약지반, 사면의 보강 등에 적합한 파일 공법</div>
             </div>
-            <div style={{ width: 1440, height: 917, left: 0, top: 2425, position: 'absolute' }}>
-                <img style={{ width: 1200, height: 613.13, left: 120, top: 283, position: 'absolute', mixBlendMode: 'darken' }} src="https://via.placeholder.com/1200x613" />
-                <div style={{ width: 1300, height: 110, left: 66, top: 87, position: 'absolute', background: '#004D7B' }} />
-                <div style={{ width: 470, height: 63, left: 105, top: 111, position: 'absolute', color: 'white', fontSize: 32, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>MICRO PILE 시공순서</div>
-            </div>
-            <div style={{ width: 1440, height: 657, left: 0, top: 1183, position: 'absolute', mixBlendMode: 'darken' }}>
-                <div style={{ width: 1300, height: 500, left: 70, top: 157, position: 'absolute', background: 'white', border: '2px black solid' }} />
-                <div style={{ width: 510, height: 63, left: 168, top: 77, position: 'absolute', color: '#363636', fontSize: 48, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>MICRO PILES 공법 특징</div>
-                <img style={{ width: 253, height: 473, left: 1050, top: 171, position: 'absolute' }} src="https://via.placeholder.com/253x473" />
-                <div style={{ width: 800, height: 177, left: 101, top: 319, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>소구경으로 대구경 파일의 지지력 확보 가능<br />압축력과 인장력을 동시에 발휘하므로 부력앙카를 겸할 수 있음<br />건설장비의 규모가 작아 건물내부, Strut 하부 등 협소한 현장에서 시공이 용이함<br />시멘트 밀크를 사용하므로 천공 및 주변 지반 균열부에 충진이 가능<br />소구경이라 Pile 간의 간격을 좁힐 수 있어 군말뚝의 지지력 감소와 부마찰력 문제를 최소화 함</div>
-                <div style={{ width: 67, height: 61, left: 70, top: 79, position: 'absolute' }}>
-                    <div style={{ width: 40.15, height: 60.57, left: 3, top: 1, position: 'absolute', background: 'rgba(103, 153, 255, 0.25)' }}></div>
-                    <div style={{ width: 40.15, height: 60.57, left: 16.65, top: 1, position: 'absolute', background: 'rgba(103, 153, 255, 0.50)' }}></div>
-                    <img style={{ width: 36, height: 60.57, left: 31, top: 1, position: 'absolute' }} src="https://via.placeholder.com/36x61" />
+            {/* 공법특징*/}
+            <div className={styles.feature_container}>
+                <div className={styles.feature_wrapper}>
+                    <div className={styles.feature_top}>
+                        <div className={styles.feature_icon} />
+                        <div className={styles.feature_text}>MICRO PILES 공법 특징</div>    
+                    </div>
+                    
+                    <div className={styles.feature_background} >
+                        <div className={styles.feature_description}>
+                            <ul>
+                                <li>소구경으로 대구경 파일의 지지력 확보 가능</li>
+                                <li>압축력과 인장력을 동시에 발휘하므로 부력앙카를 겸할 수 있음</li>
+                                <li>건설장비의 규모가 작아 건물내부, Strut 하부 등 협소한 현장에서 시공이 용이함</li>
+                                <li>시멘트 밀크를 사용하므로 천공 및 주변 지반 균열부에 충진이 가능</li>
+                                <li>소구경이라 Pile 간의 간격을 좁힐 수 있어 군말뚝의 지지력 감소와 부마찰력 문제를 최소화 함</li>
+                            </ul>
+                        </div>
+                        <div className={styles.feature_image} />
+                    </div>  
                 </div>
             </div>
-            <div style={{ width: 1440, height: 531, left: 5, top: 1894, position: 'absolute' }}>
-                <div style={{ width: 1300, height: 110, left: 61, top: 11, position: 'absolute', background: '#004D7B' }} />
-                <div style={{ width: 470, height: 63, left: 100, top: 35, position: 'absolute', color: 'white', fontSize: 32, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>MICRO PILE 적용 분야</div>
-                <img style={{ width: 564, height: 352, left: 735, top: 121, position: 'absolute', mixBlendMode: 'darken' }} src="https://via.placeholder.com/564x352" />
-                <div style={{ width: 542, height: 200, left: 100, top: 197, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>기존 구조물의 언더피닝 (Under Pinning) 시공시<br />기존건물(구조물)의 기초 보강<br />대형 굴착장비의 진입이 불가능한 협소한 지역의 기초 시공<br />압축 및 인장이 동시에 작용하는 타워, 굴뚝 및 송전탑의 기초<br />중파된 PHC PILE 보강 및 편차 발생 시<br />지반 및 굴착조건이 양호하지 않은 경우</div>
-            </div>
-            <div style={{ width: 1440, height: 1046, left: 5, top: 3414, position: 'absolute' }}>
-                <img style={{ width: 1200, height: 855.31, left: 120, top: 147, position: 'absolute' }} src="https://via.placeholder.com/1200x855" />
-                <div style={{ width: 1300, height: 110, left: 61, top: 0, position: 'absolute', background: '#004D7B' }} />
-                <div style={{ width: 470, height: 63, left: 100, top: 23, position: 'absolute', color: 'white', fontSize: 32, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>MICRO PILE 시공도면</div>
-            </div>
-            <div style={{ width: 1440, height: 1150, left: 0, top: 4576, position: 'absolute' }}>
-                <img style={{ width: 400, height: 612, left: 117, top: 199, position: 'absolute' }} src="https://via.placeholder.com/400x612" />
-                <img style={{ width: 400, height: 392.57, left: 537, top: 199, position: 'absolute' }} src="https://via.placeholder.com/400x393" />
-                <img style={{ width: 400, height: 370.21, left: 537, top: 619, position: 'absolute' }} src="https://via.placeholder.com/400x370" />
-                <img style={{ width: 400, height: 498.67, left: 958, top: 199, position: 'absolute' }} src="https://via.placeholder.com/400x499" />
-                <div style={{ width: 1300, height: 110, left: 70, top: 0, position: 'absolute', background: '#004D7B' }} />
-                <div style={{ width: 510, height: 63, left: 109, top: 24, position: 'absolute', color: 'white', fontSize: 32, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>MICRO PILE 시공설명서</div>
-            </div>
-            <div style={{ width: 410, height: 48, left: 515, top: 938, position: 'absolute', color: '#363636', fontSize: 48, fontFamily: 'Nanum Gothic', fontWeight: '700', wordWrap: 'break-word' }}>MICRO PILES 공법</div>
-            <div style={{ height: 237, paddingTop: 58, paddingBottom: 57, paddingLeft: 70, paddingRight: 474, left: 0, top: 5726, position: 'absolute', background: '#373737', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex' }}>
-                <div style={{ width: 896, alignSelf: 'stretch', position: 'relative' }}>
-                    <div style={{ left: 0, top: 0, position: 'absolute', color: 'white', fontSize: 24, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>기탄산업개발</div>
-                    <div style={{ left: 0, top: 55, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Nanum Gothic', fontWeight: '400', lineHeight: 32, wordWrap: 'break-word' }}>상호명: 기탄산업 개발 | 대표자: 정원근 | 사업자번호: 699-05-02875 | 이메일 : gittan@naver.com<br />TEL: 031-853-5028 | FAX: 0504-327-5028 | 주소: 경기도 의정부시 오목로 235 제일프라자 2층 202호</div>
+
+            {/* 적용분야 */}
+            <div className={styles.common_container}>
+                <div className={styles.common_title}>MICRO PILE 적용 분야</div>
+                <div className={styles.apply_background}>
+                    <div className={styles.apply_description}>
+                        <ul>
+                            <li>기존 구조물의 언더피닝 (Under Pinning) 시공시</li>
+                            <li>기존건물(구조물)의 기초 보강</li>
+                            <li>대형 굴착장비의 진입이 불가능한 협소한 지역의 기초 시공</li>
+                            <li>압축 및 인장이 동시에 작용하는 타워, 굴뚝 및 송전탑의 기초</li>
+                            <li>중파된 PHC PILE 보강 및 편차 발생 시</li>
+                            <li>지반 및 굴착조건이 양호하지 않은 경우</li>
+                        </ul>
+                    </div>
+                    <div className={styles.apply_image} />
                 </div>
             </div>
-            <div style={{ width: 1440, height: 104, paddingLeft: 31, paddingRight: 31, paddingTop: 22, paddingBottom: 22, left: 5, top: 0, position: 'absolute', border: '1px #646464 solid', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex' }}>
-                <div style={{ justifyContent: 'flex-start', alignItems: 'center', display: 'flex' }}>
-                    <img style={{ width: 50, height: 50 }} src="https://via.placeholder.com/50x50" />
-                    <div style={{ width: 170, color: 'black', fontSize: 28, fontFamily: 'Nanum Gothic', fontWeight: '600', wordWrap: 'break-word' }}> 기탄산업개발</div>
-                </div>
-                <div style={{ width: 360, height: 19 }} />
-                <div style={{ justifyContent: 'flex-start', alignItems: 'flex-start', gap: 18, display: 'flex' }}>
-                    <div style={{ color: 'black', fontSize: 20, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>회사소개</div>
-                    <div style={{ color: 'black', fontSize: 20, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>사업소개</div>
-                    <div style={{ color: 'black', fontSize: 20, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>시공실적</div>
-                    <div style={{ color: 'black', fontSize: 20, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>견적문의</div>
-                    <div style={{ color: 'black', fontSize: 20, fontFamily: 'Nanum Gothic', fontWeight: '400', wordWrap: 'break-word' }}>공식블로그</div>
+
+            {/* 시공순서 */}
+            <div className={styles.common_container}>
+                <div className={styles.common_title}>MICRO PILE 시공순서</div>
+                <div className={styles.build_background}>
+                    <div className={styles.build_image}></div>
                 </div>
             </div>
-        </div>
+            
+            {/* 시공도면 */}
+            <div className={styles.common_container}>
+                <div className={styles.common_title}>MICRO PILE 시공도면</div>
+                <div className={styles.plan_background}>
+                    <div className={styles.plan_image}></div>
+                </div>
+            </div>
+
+            {/* 시공설명서 */}
+            <div className={styles.common_container}>
+                <div className={styles.common_title}>MICRO PILE 시공도면</div>
+                <div className={styles.explain_background}>
+                <div className={styles.explain_image} style={{backgroundImage : `url(/micro_explain_img4.svg)`}}></div>    
+                    <div className={styles.explain_image_container}>
+                        <div className={styles.explain_image} style={{backgroundImage : `url(/micro_explain_img3.svg)`}}></div>
+                        <div className={styles.explain_image} style={{backgroundImage : `url(/micro_explain_img2.svg)`}}></div>
+                    </div>
+                <div className={styles.explain_image} style={{backgroundImage : `url(/micro_explain_img1.svg)`}}></div>
+                </div>
+            </div>
+        
+        <Footer />
+    </div>
     );
 };
 
