3142080f05dd622aac2eb4d7fedcb9849c9e0219
HUMBLE25 <wlsrua1007@gmail.com>
2024-03-02 15:22:56 +0900

    FIX mainpage img size & header layout

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,12 @@ const Home: React.FC = () => {
   const performance_img2 = '/performance_img2.svg';
   const performance_img3 = '/performance_img3.svg';
   const performance_img4 = '/performance_img4.svg';
+  const performance_img5 = '/performance_img1.svg';
+  const performance_img6 = '/performance_img2.svg';
+  const performance_img7 = '/performance_img3.svg';
+  const performance_img8 = '/performance_img4.svg';
   const main_image = '/main_img.svg';
-  const bannerText = `내진보강공사 및 마이크로파일 & 헬리컬파일 시공 전문 업체\n전국 전문가팀 파견시공\n내진보강공사 무료상담`;
+  const bannerText = `내진보강공사 및 마이크로파일\n헬리컬파일 시공 전문 업체\n전국 전문가팀 파견시공\n내진보강공사 무료상담`;
   
   return (
     <div className={styles.wrapper}>
@@ -27,6 +31,12 @@ const Home: React.FC = () => {
       <div className={styles.main_container}>
         <div className={styles.performance_text}>주요 시공 실적</div>
         <div className={styles.flex_container}>
+          <Item src={performance_img1}  text='강원OO초등학교' />
+          <Item src={performance_img2}  text='거제OO학교' />
+          <Item src={performance_img3}  text='꿈이랑도서관' />
+          <Item src={performance_img4}  text='대구OO도서관' />
+        </div>
+        <div className={styles.flex_container_sub}>
           <Item src={performance_img1}  text='강원OO초등학교' />
           <Item src={performance_img2}  text='거제OO학교' />
           <Item src={performance_img3}  text='꿈이랑도서관' />
