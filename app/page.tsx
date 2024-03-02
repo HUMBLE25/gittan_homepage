cfbaffb45bc82dd7292c47deb59d2e8b614a3641
HUMBLE25 <wlsrua1007@gmail.com>
2024-03-02 17:36:57 +0900

    FIX header sizing & FIX popup rayout

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,10 @@ const Home: React.FC = () => {
   const performance_img2 = '/performance_img2.svg';
   const performance_img3 = '/performance_img3.svg';
   const performance_img4 = '/performance_img4.svg';
-  const performance_img5 = '/performance_img1.svg';
-  const performance_img6 = '/performance_img2.svg';
-  const performance_img7 = '/performance_img3.svg';
-  const performance_img8 = '/performance_img4.svg';
+  const performance_img5 = '/cPerformance4.svg';
+  const performance_img6 = '/cPerformance7.svg';
+  const performance_img7 = '/cPerformance8.svg';
+  const performance_img8 = '/cPerformance9.svg';
   const main_image = '/main_img.svg';
   const bannerText = `내진보강공사 및 마이크로파일\n헬리컬파일 시공 전문 업체\n전국 전문가팀 파견시공\n내진보강공사 무료상담`;
   
@@ -37,10 +37,10 @@ const Home: React.FC = () => {
           <Item src={performance_img4}  text='대구OO도서관' />
         </div>
         <div className={styles.flex_container_sub}>
-          <Item src={performance_img1}  text='강원OO초등학교' />
-          <Item src={performance_img2}  text='거제OO학교' />
-          <Item src={performance_img3}  text='꿈이랑도서관' />
-          <Item src={performance_img4}  text='대구OO도서관' />
+          <Item src={performance_img5}  text='성남 OO중학교' />
+          <Item src={performance_img6}  text='화성 OO중학교' />
+          <Item src={performance_img7}  text='철원 OO고등학교' />
+          <Item src={performance_img8}  text='홍천 OO중학교' />
         </div>
       </div>
       
