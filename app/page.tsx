0d035d6e13ca6305ac386ed27e6df32fcfe8da69
HUMBLE25 <wlsrua1007@gmail.com>
2024-03-10 13:37:19 +0900

    change main page to full screen scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import styles from '@/src/styles/page.module.css';
 import Banner from '@/src/components/Banner';
 import '@/src/styles/globals.css'
@@ -18,34 +18,67 @@ const Home: React.FC = () => {
   const performance_img6 = '/cPerformance7.svg';
   const performance_img7 = '/cPerformance8.svg';
   const performance_img8 = '/cPerformance9.svg';
-  const [bannerText, setBannerText] = useState(`내진보강공사 및 마이크로파일 & 헬리컬파일 시공 전문 업체\n전국 전문가팀 파견시공\n내진보강공사 무료상담`);
-  const main_image = '/main_img.svg';
+
+  const main_image = ['/mainpage_img1.svg', '/mainpage_img2.svg', '/mainpage_img3.svg', '/mainpage_img4.svg'];
+  const main_texts = [
+    '안전을 최우선으로 하는 전문건설기업 기탄산업개발 \n 성공적인 시공사례들로 믿을 수 있는 기업',
+    '성실과 책임을 다하는 전문건설기업 기탄산업개발 \n 전국 전문가팀 파견시공 가능한 기업',
+    '내진보강시공 전문건설기업 기탄산업개발 \n Micro Piles & Helix Piles 전문시공업체',
+    '새로운 기술을 선도하는 전문건설기업 기탄산업개발 \n 축척된 기술로 전문팀의 내진공사 전문시공'
+];
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 480) { 
-        setBannerText(`내진보강공사 및 마이크로파일 \n 헬리컬파일 시공 전문 업체\n전국 전문가팀 파견시공\n내진보강공사 무료상담`);
-      } else {
-        setBannerText(`내진보강공사 및 마이크로파일 & 헬리컬파일 시공 전문 업체\n전국 전문가팀 파견시공\n내진보강공사 무료상담`);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    handleResize();
-
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % main_image.length);
+    }, 5000);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      clearInterval(interval); 
     };
   }, []);
+
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const nextSectionRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const onScroll = (e: WheelEvent) => {
+      e.preventDefault();
+      if (e.deltaY > 0) {
+        if (nextSectionRef.current){
+          nextSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    };
   
+    const div = sectionRef.current;
+    if (div) {
+      div.addEventListener('wheel', onScroll, { passive: false });
+    }
+  
+    return () => {
+      if (div) {
+        div.removeEventListener('wheel', onScroll);
+      }
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
-
       <Header/>
       <Estimate/>
-      <Banner img={main_image} text={bannerText}/>
-
+      <div className={styles.banner} ref={sectionRef} >
+          <div className={styles.main_image}  style={{backgroundImage:`url(${main_image[index]})`}}/>
+          <div className={styles.overlay}/>
+          <div className={styles.overlay_text}>{main_texts[index]}</div>
+          <div className={styles.side_bar_wrapper}>
+            {Array(4).fill(null).map((_, i) => (
+              <div key={i} className={styles.side_bar_item} style={{backgroundColor: i === index ? 'hsl(0, 0%, 100%)' : 'hsl(0, 0%, 20%)'}}/>
+            ))}
+          </div>
+      </div>
+      <div ref={nextSectionRef}>
+        <div className={styles.main_body_container}>
+              <div className={styles.main_body_text_container}></div>
+        </div>
       <div className={styles.main_container}>
         <div className={styles.performance_text}>주요 시공 실적</div>
         <div className={styles.flex_container}>
@@ -61,6 +94,9 @@ const Home: React.FC = () => {
           <Item src={performance_img8}  text='홍천 OO중학교' />
         </div>
       </div>
+      </div>
+
+
       
       <Footer/>
     </div>
