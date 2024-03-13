12b7089fbe1b7baf18d18b302749ee1f2cc4e62e
HUMBLE25 <wlsrua1007@gmail.com>
2024-03-13 16:52:08 +0900

    Change admin page rayout

--- src/pages/api/gallery.ts
+++ src/pages/api/gallery.ts
@@ -8,11 +8,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         const { title, author, content, file } = req.body;
         console.log("==== hi i'm post ====")
         console.log(req.body)
-        // Base64 데이터에서 실제 파일 데이터를 추출합니다.
+        
         const base64Data = file.replace(/^data:([A-Za-z-+/]+);bas e64,/, '');
         fs.writeFileSync(path.join(__dirname, `${title}.jpg`), base64Data, 'base64');
 
-        console.log(title, author, content); // 폼 데이터 접근
+        console.log(title, author, content); 
         res.status(200).json({ message: 'Success' });
     } else {
         // GET 요청 처리
